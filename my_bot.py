d4a06c8d576f5ac6f776269cf2003dd9ff5fa7e3
Mr-afk-cell <kboxser2026@kehillahstudent.org>
2025-02-09 10:34:35 -0800

    discord bot

--- my_bot.py
+++ my_bot.py
@@ -10,6 +10,14 @@ This function will be called every time anyone says anything on a channel where 
 def should_i_respond(user_message, user_name):
   if "favourite color" in user_message:
     return True
+  elif "what is your name" in user_message:
+    return True
+  elif "what is your favourite food" in user_message:
+    return True
+  elif "how old are you" in user_message:
+    return True
+  elif "what is your favourite class" in user_message:
+    return True
   else:
     return False
 
@@ -23,4 +31,13 @@ This function will be called every time the `should_i_respond` function returns 
 * You can have the bot respond differently to different messages and users
 """
 def respond(user_message, user_name):
-  return "my favourite color is green"
+  if "what is your name" in user_message:
+    return "my name is chat-bot"
+  elif "favourite color" in user_message:
+    return "my favourite color is green"
+  elif "what is your favourite food" in user_message:
+    return "my favourite food is pizza"
+  elif "how old are you" in user_message:
+    return "I am 156 years old"
+  elif "what is your favourite class" in user_message:
+    return "my favourite class is Computer Science" 
